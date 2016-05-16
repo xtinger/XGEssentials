c45ac456f0a3bfe630f3d5147d20b3b246a171fc
xtinger <xtinger@bk.ru>
2016-05-16 15:16:12 +0300

    1

---
+++ XGEssentials/Code/Strings/NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+MD5.h
+//  Lini
+//
+//  Created by Denis Voronov on 09/04/14.
+//  Copyright (c) 2014 Fold & Spine. All rights reserved.
+//
+
+@import Foundation;
+
+@interface NSString (MD5)
+
+- (NSString *)MD5String;
+
+@end
