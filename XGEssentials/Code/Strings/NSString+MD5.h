22b055838ffe4dcc4db3bb44e1c43568b63e3ffc
xtinger <xtinger@bk.ru>
2016-05-16 15:15:14 +0300

    MD5

---
+++ XGEssentials/Code/Strings/NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+MD5.h
+//  XGEssentials
+//  https://github.com/xtinger/XGEssentials
+//
+//  Created by Denis Voronov on 09/04/14.
+//
+
+@import Foundation;
+
+@interface NSString(XG_MD5)
+
+- (NSString *)MD5String;
+
+@end
