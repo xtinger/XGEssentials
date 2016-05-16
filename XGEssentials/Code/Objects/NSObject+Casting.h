be3057053254ef1a031520d7b87bdfc1e19bc1ce
xtinger <xtinger@bk.ru>
2016-05-16 15:29:04 +0300

    1

---
+++ XGEssentials/Code/Objects/NSObject+Casting.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+Casting.h
+//  MeetingAt
+//
+//  Created by Denis Voronov on 28/05/15.
+//  Copyright (c) 2015 Emanor Finance Limited. All rights reserved.
+//
+
+@import Foundation;
+
+@interface NSObject (Casting)
+
++ (instancetype)cast:(id)obj;
+
+@end
