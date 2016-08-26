2bd8e947770d965ccd95fe06393541866a7a9a9f
xtinger <xtinger@bk.ru>
2016-08-26 15:16:51 +0300

    контакты сообщества

---
+++ XGEssentials/Code/UI/UIView+XGRemoveConstraints.h
@@ -0,0 +1,15 @@
+//
+//  UIView+RemoveConstraints.h
+//  MeetingAt
+//
+//  Created by Denis Voronov on 26/08/16.
+//  Copyright © 2016 Emanor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (XGRemoveConstraints)
+
+- (void)removeAllConstraints;
+
+@end
