53a951dd833c48bd6e459e9cc4f12dc1e97634ea
xtinger <xtinger@bk.ru>
2016-12-07 17:49:59 +0300

    sidebar badges

---
+++ XGEssentials/Code/UI/XGInsetLabel.m
@@ -0,0 +1,33 @@
+//
+//  XGInsetLabel.m
+//  MeetingAt
+//
+//  Created by Denis Voronov on 07/12/16.
+//  Copyright © 2016 Emanor. All rights reserved.
+//
+
+#import "XGInsetLabel.h"
+
+@implementation XGInsetLabel
+
+- (id)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.edgeInsets = UIEdgeInsetsMake(0, 0, 0, 0);
+    }
+    return self;
+}
+
+- (void)drawTextInRect:(CGRect)rect {
+    [super drawTextInRect:UIEdgeInsetsInsetRect(rect, self.edgeInsets)];
+}
+
+- (CGSize)intrinsicContentSize
+{
+    CGSize size = [super intrinsicContentSize];
+    size.width  += self.edgeInsets.left + self.edgeInsets.right;
+    size.height += self.edgeInsets.top + self.edgeInsets.bottom;
+    return size;
+}
+
+@end
